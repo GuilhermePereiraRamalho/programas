2018e1e847310e3fc411fe7647140356c96b50d7
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2023-07-04 11:56:21 -0300

    Add files via upload

--- #85 - Lista com pares e impares.py
+++ #85 - Lista com pares e impares.py
@@ -1,18 +1,14 @@
 #85 - Lista com pares e impares
 
-total = list()
-par = list()
-impar = list()
+total = [[], []]
+
 for c in range(0,7):
     n = int(input(f'Digite o {c+1}o valor: '))
     if n % 2 == 0:
-        par.append(n)
+        total[0].append(n)
     else:
-        impar.append(n)      
-total.append(par[:])
-par.clear()
-total.append(impar[:])
-impar.clear()
+        total[1].append(n)      
+
 print('-='*30)
 print(f'O valores pares digitados foram: {sorted(total[0])}')
 print(f'O valores impares digitados foram: {sorted(total[1])}')
