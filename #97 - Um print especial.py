76d0300dd60df20f7eae426be332ccb1fcc92bc6
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2023-07-06 09:18:28 -0300

    Add files via upload

---
+++ #97 - Um print especial.py
@@ -0,0 +1,10 @@
+#97 - Um print especial
+def escreva(str):
+    print('~'*(len(str)+4))
+    print(f'  {str}')
+    print('~'*(len(str)+4))
+
+
+escreva('Gustavo Guanabara')
+escreva('Curso de Python no YouTube')
+escreva('CeV')
