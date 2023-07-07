2eb437098bd38778a6d7e4b90d65579804bc283c
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2023-07-07 15:50:51 -0300

    Add files via upload

---
+++ #109 - Exercitando módulos em python.py
@@ -0,0 +1,5 @@
+#109 - Exercitando módulos em python
+import Moeda
+
+p = float(input('Digite o preço: R$ '))
+Moeda.resumo(p,80,35)
--- Moeda.py
+++ Moeda.py
@@ -1,14 +1,32 @@
-def metade(preço):
-    return preço/2
+def metade(preço, formato = False):
+    res = preço/2
+    return res if formato is False else moeda(res)
 
 
-def dobro(preço):
-    return preço*2
+def dobro(preço, formato = False):
+    res = preço*2
+    return res if formato is False else moeda(res)
 
 
-def aumentar(preço):
-    return preço*1.1
+def aumentar(preço, a=0, formato = False):
+    res = preço*(1+(a/100))
+    return res if formato is False else moeda(res)
 
 
-def diminuir(preço):
-    return preço*0.87
+def diminuir(preço,d=0, formato = False):
+    res = preço*(1-(d/100))
+    return res if formato is False else moeda(res)
+
+def moeda(preço = 0, moeda = 'R$'):
+    return f'{moeda} {preço:>.2f}'.replace('.',',')
+
+
+def resumo(preço=0, a=0, d=0):
+    print('-'*30)
+    print(F'{"RESUMO DO VALOR":^30}')
+    print('-'*30)
+    print(f'Preço analisado: \t{moeda(preço)}')
+    print(f'Dobro do preço: \t{dobro(preço, True)}')
+    print(f'Metade do preço: \t{metade(preço, True)}')
+    print(f'{a}% de aumento: \t{aumentar(preço,a,True)}')
+    print(f'{d}% de redução: \t{diminuir(preço,d,True)}')
